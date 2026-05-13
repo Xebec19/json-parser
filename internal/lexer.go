361f2a3e008a437704b2fed7c8b1e60948e12f26
xebec19 <rohandeveloper106@gmail.com>
2026-05-13 21:57:22 +0530

    wip lexer

---
+++ internal/lexer.go
@@ -0,0 +1,21 @@
+package internal
+
+// re := regexp.MustCompile(`\d+`)
+
+// re.MatchString("abc123")
+// re.Match([]byte("abc123"))
+
+// const T = map[string]string{
+// 	"Number": "Number",
+
+// }
+
+func Lexer(input string) {
+
+	i := 0
+	tokens := []string{}
+
+	for i < len(input) {
+
+	}
+}
