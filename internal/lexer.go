1f8639f17827c9cba4cdaa26bc64b2ad5b7324db
xebec19 <rohandeveloper106@gmail.com>
2026-06-02 22:12:37 +0530

    feat: lexer for math expr

--- internal/lexer.go
+++ internal/lexer.go
@@ -1,5 +1,7 @@
 package internal
 
+import "strings"
+
 // re := regexp.MustCompile(`\d+`)
 
 // re.MatchString("abc123")
@@ -10,12 +12,68 @@ package internal
 
 // }
 
-func Lexer(input string) {
+var SYMBOLS = map[string]string{
+	"NUMBER": "NUMBER",
+	"PLUS":   "PLUS",
+	"SUBST":  "SUBSTRACT",
+	"MULT":   "MULTIPLY",
+	"DIV":    "DIVIDE",
+	"LPAREN": "LPAREN",
+	"RPAREN": "RPAREN",
+}
 
-	i := 0
-	tokens := []string{}
+type TokenList struct {
+	Key string
+	Val string
+}
 
-	for i < len(input) {
+// Lexer takes a string and convert it
+// to TokenList struct
+func Lexer(input string) []TokenList {
+
+	tokens := []TokenList{}
+	inputList := strings.Split(input, "")
+
+	for _, elem := range inputList {
+
+		if isNumber(elem) {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["NUMBER"],
+				Val: elem,
+			})
+		} else if elem == "+" {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["PLUS"],
+				Val: elem,
+			})
+		} else if elem == "-" {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["SUBST"],
+				Val: elem,
+			})
+		} else if elem == "*" {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["MULT"],
+				Val: elem,
+			})
+		} else if elem == "/" {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["DIV"],
+				Val: elem,
+			})
+		} else if elem == "(" {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["LPAREN"],
+				Val: elem,
+			})
+		} else if elem == ")" {
+			tokens = append(tokens, TokenList{
+				Key: SYMBOLS["RPAREN"],
+				Val: elem,
+			})
+		}
 
 	}
+
+	return tokens
 }
