06b5a9624c75e6b39219aa4d5a163b5bc0632498
xebec19 <rohandeveloper106@gmail.com>
2026-06-06 21:50:30 +0530

    wip parser

--- internal/parser.go
+++ internal/parser.go
@@ -1,5 +1,28 @@
 package internal
 
-func expr()   {} // for + | -
-func term()   {} // for * | /
-func factor() {} // Number | '(' ')'
+type Operator string
+
+var (
+	PLUS   Operator = "PLUS"
+	SUBST  Operator = "SUBSTRACT"
+	MULT   Operator = "MULTIPLY"
+	DIV    Operator = "DIVIDE"
+	LPAREN Operator = "LPAREN"
+	RPAREN Operator = "RPAREN"
+)
+
+type AST struct {
+	left  *AST
+	opt   Operator
+	right *AST
+}
+
+func expr(tokens []TokenList) AST {
+	return term(tokens)
+}
+
+func term(tokens []TokenList) AST {
+
+}                  // for + | -
+func factor() AST  {} // for * | /
+func primary() AST {} // Number | '(' ')'
